e2a414e4fbcc1ae0239328cfe84e6ef4e666d225
Rishikesh Sadanandan <rishikesh.sadanandan@gmail.com>
2025-05-11 18:32:54 -0400

    home metadata

--- apps/site/app/routes/home.tsx
+++ apps/site/app/routes/home.tsx
@@ -1,10 +1,10 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "site.keshsad.com" },
+    { name: "Rishi's personal site", content: "Welcome to Rishi's personal site!" },
   ];
 }
 
