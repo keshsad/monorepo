0330d10fdaf33ce1ee2586378d5596c82ed222bb
Rishikesh Sadanandan <rishikesh.sadanandan@gmail.com>
2025-05-11 18:17:13 -0400

    add react infra

--- infra/app.ts
+++ infra/app.ts
@@ -1,6 +1,28 @@
 import { router } from "./shared";
 import { subdomain } from "./stage";
 
+export const api = new sst.aws.Function("Api", {
+  handler: "apps/api/main.go",
+  runtime: "go",
+  url: {
+    router: {
+      instance: router,
+      domain: subdomain("api")
+    }
+  },
+})
+
+export const site = new sst.aws.React("WebSite", {
+  path: "apps/site",
+  router: {
+    instance: router,
+    domain: subdomain("site")
+  },
+  environment: {
+    API_URL: api.url
+  },
+})
+
 export const rootRedirect = new sst.aws.Function("RootRedirect", {
   handler: "packages/functions/src/root-redirect.handler",
   url: true
@@ -14,14 +36,3 @@ export const pathRedirect = new sst.aws.Function("PathRedirect", {
 router.route("/", rootRedirect.url)
 router.route("/*", rootRedirect.url)
 
-
-export const api = new sst.aws.Function("Api", {
-  handler: "apps/api/main.go",
-  runtime: "go",
-  url: {
-    router: {
-      instance: router,
-      domain: subdomain("api")
-    }
-  },
-})
