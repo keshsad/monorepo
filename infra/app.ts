d4f7879c8f875b9c6364cb29fffcd56a281ed6eb
Rishikesh Sadanandan <rishikesh.sadanandan@gmail.com>
2025-05-19 17:54:29 -0400

    rm path redirect (costs)

--- infra/app.ts
+++ infra/app.ts
@@ -7,7 +7,7 @@ export const api = new sst.aws.Function("Api", {
   url: {
     router: {
       instance: router,
-      domain: subdomain("api")
+      domain: subdomain("api"),
     }
   },
 })
@@ -31,11 +31,11 @@ export const rootRedirect = new sst.aws.Function("RootRedirect", {
   url: true
 })
 
-export const pathRedirect = new sst.aws.Function("PathRedirect", {
-  handler: "packages/functions/src/path-redirect.handler",
-  url: true
-})
+//export const pathRedirect = new sst.aws.Function("PathRedirect", {
+//  handler: "packages/functions/src/path-redirect.handler",
+//  url: true
+//})
 
 router.route("/", rootRedirect.url)
-router.route("/*", rootRedirect.url)
+//router.route("/*", pathRedirect.url)
 
