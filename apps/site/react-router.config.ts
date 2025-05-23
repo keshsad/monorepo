b1994459d61a9e1ed61b264e28ff4a86f770ac3c
Rishikesh Sadanandan <rishikesh.sadanandan@gmail.com>
2025-05-23 13:44:37 -0400

    prerender links except writing

--- apps/site/react-router.config.ts
+++ apps/site/react-router.config.ts
@@ -4,4 +4,8 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  // return list of URLs to prerender at build tim
+  async prerender() {
+    return ["/", "/about", "/work", "/contact"];
+  }
 } satisfies Config;
