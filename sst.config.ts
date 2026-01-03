d691fb9aef3f357e3751b3b217ae6156a9b1fdf0
Rishikesh Sadanandan <rishikesh.sadanandan@gmail.com>
2026-01-03 03:39:45 -0500

    feat: added custom domain and multi-stage deployments
    sveltekit personal site resolves any url matching:
    
    - keshsad.com
    - *.keshsad.com
    - dev.keshsad.com
    - *.dev.keshsad.com

--- sst.config.ts
+++ sst.config.ts
@@ -16,8 +16,35 @@ export default $config({
     };
   },
   async run() {
+    const isPermanentStage = ["production", "dev"].includes($app.stage);
+
+    const domain =
+      $app.stage === "production"
+        ? "keshsad.com"
+        : $app.stage === "dev"
+          ? "dev.keshsad.com"
+          : `${$app.stage}.dev.keshsad.com`;
+
+    const router = isPermanentStage
+      ? new sst.aws.Router("Router", {
+          domain: {
+            name: domain,
+            aliases: [`*.${domain}`],
+            dns: sst.cloudflare.dns(),
+          },
+        })
+      : sst.aws.Router.get("Router", "E2X0XGSO05RTZO");
+
     new sst.aws.SvelteKit("PersonalSite", {
       path: "apps/site",
+      router: {
+        instance: router,
+      },
     });
+
+    return {
+      routerUrl: router.url,
+      routerDistributionID: router.distributionID,
+    };
   },
 });
